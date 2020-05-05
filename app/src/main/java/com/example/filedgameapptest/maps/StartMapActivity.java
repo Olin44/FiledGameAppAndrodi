b0541d4bbcc22201c6662bf994c00806b52ea1d7
Monika Dąbrowska <monikad0602@gmail.com>
2020-05-05 13:52:37 +0200

    Basic MapsActivity implementation

--- app/src/main/java/com/example/filedgameapptest/maps/StartMapActivity.java
+++ app/src/main/java/com/example/filedgameapptest/maps/StartMapActivity.java
@@ -8,11 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.filedgameapptest.MainActivity;
 import com.example.filedgameapptest.R;
+import com.example.filedgameapptest.users.login.ui.login.LoginActivity;
+import com.example.filedgameapptest.users.register.RegisterActivity;
 
-public class StartMapActivity extends AppCompatActivity {
+public class StartMapActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button btnRequest;
+    private Button btnStartNewMapActivity;
     private TextView txtResponse;
     private String url;
 
@@ -24,32 +28,48 @@ public class StartMapActivity extends AppCompatActivity {
         url = incomingIntent.getStringExtra("url");
 
         initViews();
+        txtResponse.setText(url);
+
+
+    }
+
+    private void RequestMap() {
+        //
+        //tu sobie piszesz co chcesz zrobić na klikniecie button
+        //
+
+        //tym możesz sobie wyświetlić wynik
+        String response = "";
+        txtResponse.setText(response);
+
+        //Chciałabym aby na request (który się będzie dział automatycznie a nie na kliknięcie) pobierało mape i tworzylo mi liste obiektow na mapie
+        // pozniej po kliknieciu 'start' bedzie przechodzilo do aktywnosci juz z mapa google (MapsActivity) i lokalizacja obiektu
+
+    }
+
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btnRequest:
+                RequestMap();
+                break;
+            case R.id.btnStartNewMapActivity:
+                //startActivity(new Intent(StartMapActivity.this, MapsActivity.class));
+                break;
+
+        }
+
     }
 
     private void initViews() {
         btnRequest = findViewById(R.id.btnRequest);
+        btnStartNewMapActivity = findViewById(R.id.btnStartNewMapActivity);
         txtResponse = findViewById(R.id.txtResponse);
 
-        txtResponse.setText(url);
+        btnRequest.setOnClickListener(this);
+        btnStartNewMapActivity.setOnClickListener(this);
 
-
-        btnRequest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                //
-                //tu sobie piszesz co chcesz zrobić na klikniecie button
-                //
-
-                //tym możesz sobie wyświetlić wynik
-                String response = "";
-                txtResponse.setText(response);
-
-                //Chciałabym aby na request (który się będzie dział automatycznie a nie na kliknięcie) pobierało mape i tworzylo mi liste obiektow na mapie
-                // pozniej po kliknieciu 'start' bedzie przechodzilo do aktywnosci juz z mapa google i lokalizacja obiektu
-
-
-            }
-        });
     }
+
+
 }
